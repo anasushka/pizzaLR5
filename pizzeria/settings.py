612ebb57148c4e3e26e7bd2dec8dafabf59d4c93
Ivan <ivalsaf@gmail.com>
2026-05-26 00:38:07 +0300

    fixed trusted origins

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -8,6 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-pizzeria-dev-key-2024')
 DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0').split(',')
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://localhost:8000').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
